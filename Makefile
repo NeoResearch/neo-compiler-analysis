770317878d17e835cf6c6cfdcf9628af10fdbec6
Igor Machado <igor.machado@gmail.com>
2018-03-07 19:12:36 -0300

    study1 basis

---
+++ Makefile
@@ -0,0 +1,5 @@
+study1:
+	g++ study1.cpp Scanner++/Scanner.cpp -o appStudy1
+
+run:
+	./appStudy1
---
+++ study1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include "Scanner++/Scanner.h"
+#include<vector>
+#include<sstream>
+
+using namespace std;
+using namespace scannerpp;
+
+vector<string> opcodes(Scanner& scanner) {
+   vector<string> vs;
+   while(scanner.hasNext()) {
+     string op = scanner.next();
+     scanner.nextLine();
+     vs.push_back(op);
+   }
+   return vs;
+}
+
+
+void study1(string filebase) {
+    stringstream spython;
+    spython << "Samples/"+ filebase << ".py.opcodes";
+    stringstream scs;
+    scs << "Samples/" + filebase << ".cs.opcodes";
+
+    cout << "open: " << spython.str() << endl; 
+    Scanner scanFilePy(new File(spython.str()));
+    vector<string> vop_py = opcodes(scanFilePy);
+    for(unsigned i=0; i<vop_py.size(); i++)
+       cout << vop_py[i] << " ";
+    cout << endl;
+
+    cout << "open: " << scs.str() << endl; 
+    Scanner scanFileCs(new File(scs.str()));
+    vector<string> vop_cs = opcodes(scanFileCs);
+
+    cout << "RESULT1\tpy\t" << vop_py.size() << "\tcs\t" << vop_cs.size() << endl;
+
+}
+
+
+int main() {
+
+   string filelist="filelist.txt";
+
+   Scanner scanner(new File(filelist));
+
+   while(scanner.hasNext()) {
+      string path = scanner.nextLine();
+      cout << path << endl;
+      study1(path);
+   }
+
+   return 0;
+}
+
+
